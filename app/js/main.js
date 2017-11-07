d98935c1389a7941012e1d2923fb6f2b8630b00c
Garland <garlandpotts1@gmail.com>
2017-11-07 17:37:14 -0500

    Started

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // if ($(".content").attr("class").includes("is-subscriber")) {
+    //     console.log("subscriber")
+    //     $(".content").css("padding-top","0")
+    // }
+});
+console.log("working")
