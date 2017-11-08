3c64a028d25584af018f3a361cbd2b68aca608a6
Garland <garlandpotts1@gmail.com>
2017-11-08 12:21:47 -0500

    Changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // if ($(".content").attr("class").includes("is-subscriber")) {
     //     console.log("subscriber")
     //     $(".content").css("padding-top","0")
     // }
 });
-console.log("working")
+// console.log("working")
+//
+// $('body').on('mousewheel DOMMouseScroll', function(e){
+//   if(typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+//       console.log()
+//     if(e.originalEvent.detail > 25) {
+//       console.log('Down1');
+//   } else if(e.originalEvent.detail < -25){
+//         console.log('Up1');
+//     }
+//   } else if (typeof e.originalEvent.wheelDelta == 'number') {
+//       console.log(e.originalEvent.wheelDelta)
+//
+//     if(e.originalEvent.wheelDelta < 25) {
+//         console.log('Down2');
+//     } else if(e.originalEvent.wheelDelta > -25) {
+//         console.log('Up2');
+//     }
+//   }
+// });
