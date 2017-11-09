504851b1da58bcd5b05bb74cbcba8075f1863347
Garland <garlandpotts1@gmail.com>
2017-11-09 15:09:48 -0500

    Intro/footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,54 @@
+var _ = require('lodash');
+
 $(document).ready(function() {
     // if ($(".content").attr("class").includes("is-subscriber")) {
     //     console.log("subscriber")
     //     $(".content").css("padding-top","0")
     // }
+    if ($(window).width() <= 520) {
+        console.log($(window).width())
+        $(".heroNav").hover(
+            function() {
+                $(".parts").css("display", "block");
+                setTimeout(function() {
+                    $(".parts").addClass("dropDown");
+                }, 100);
+            },
+            function() {
+                $(".parts").removeClass("dropDown");
+                setTimeout(function() {
+                    $(".parts").css("display", "");
+                }, 400);
+            }
+        );
+    }
 });
-console.log("working")
 
-$('body').on('mousewheel DOMMouseScroll', function(e) {
-    if($(window).scrollTop() > $(window).height()) {
-        if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-            if (e.originalEvent.detail > 25) {
-                $(".header").addClass("scrollIn")
-            } else if (e.originalEvent.detail < -25) {
-                if($(".header").attr("class").includes("scrollIn")) {
-                    $(".header").addClass('scrollOut');
-                    setTimeout(function() {
-                        $(".header").removeClass("scrollIn").removeClass('scrollOut');
-                    }, 500);
-                }
+
+$(window).scroll(_.throttle(slowscroll, 100));
+var lastScrollTop = 0;
+
+function slowscroll() {
+    var st = $(this).scrollTop();
+    if ($(window).scrollTop() > $(window).height()) {
+        if (st > lastScrollTop) {
+            if ($(".header").attr("class").includes("scrollIn")) {
+                $(".header").addClass('scrollOut');
+                setTimeout(function() {
+                    $(".header").removeClass("scrollIn").removeClass('scrollOut');
+                }, 500);
             }
-        } else if (typeof e.originalEvent.wheelDelta == 'number') {
-
-            if (e.originalEvent.wheelDelta > 25) {
+        } else {
                 $(".header").addClass("scrollIn")
-            } else if (e.originalEvent.wheelDelta < -25) {
-                if($(".header").attr("class").includes("scrollIn")) {
-                    $(".header").addClass('scrollOut');
-
-                    setTimeout(function() {
-                        $(".header").removeClass("scrollIn").removeClass('scrollOut');
-                    }, 500);
-                }
-            }
         }
     }
+    lastScrollTop = st;
 
-});
+
+    if($(".subHead").offset().top <= $(window).scrollTop()){
+        if($(".stickynav").length <1) {
+            $("body").append("<div class='stickynav'></div>")
+        }
+
+    }
+}
