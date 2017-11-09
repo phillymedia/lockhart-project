45727429939fc83865417e31d8f7c4dc41bd2059
Garland <garlandpotts1@gmail.com>
2017-11-09 10:19:15 -0500

    Footer

--- app/js/main.js
+++ app/js/main.js
@@ -4,23 +4,35 @@ $(document).ready(function() {
     //     $(".content").css("padding-top","0")
     // }
 });
-// console.log("working")
-//
-// $('body').on('mousewheel DOMMouseScroll', function(e){
-//   if(typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
-//       console.log()
-//     if(e.originalEvent.detail > 25) {
-//       console.log('Down1');
-//   } else if(e.originalEvent.detail < -25){
-//         console.log('Up1');
-//     }
-//   } else if (typeof e.originalEvent.wheelDelta == 'number') {
-//       console.log(e.originalEvent.wheelDelta)
-//
-//     if(e.originalEvent.wheelDelta < 25) {
-//         console.log('Down2');
-//     } else if(e.originalEvent.wheelDelta > -25) {
-//         console.log('Up2');
-//     }
-//   }
-// });
+console.log("working")
+
+$('body').on('mousewheel DOMMouseScroll', function(e) {
+    if($(window).scrollTop() > $(window).height()) {
+        if (typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
+            if (e.originalEvent.detail > 25) {
+                $(".header").addClass("scrollIn")
+            } else if (e.originalEvent.detail < -25) {
+                if($(".header").attr("class").includes("scrollIn")) {
+                    $(".header").addClass('scrollOut');
+                    setTimeout(function() {
+                        $(".header").removeClass("scrollIn").removeClass('scrollOut');
+                    }, 500);
+                }
+            }
+        } else if (typeof e.originalEvent.wheelDelta == 'number') {
+
+            if (e.originalEvent.wheelDelta > 25) {
+                $(".header").addClass("scrollIn")
+            } else if (e.originalEvent.wheelDelta < -25) {
+                if($(".header").attr("class").includes("scrollIn")) {
+                    $(".header").addClass('scrollOut');
+
+                    setTimeout(function() {
+                        $(".header").removeClass("scrollIn").removeClass('scrollOut');
+                    }, 500);
+                }
+            }
+        }
+    }
+
+});
