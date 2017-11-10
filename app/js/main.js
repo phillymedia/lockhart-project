53be9d8b4408847c26229e7023cb4ec4d1ec05f8
jared-whalen <jwhalen@philly.com>
2017-11-10 10:35:43 -0500

    Resized images

--- app/js/main.js
+++ app/js/main.js
@@ -2,53 +2,86 @@ var _ = require('lodash');
 var AOS = require('aos');
 
 $(document).ready(function() {
-    // if ($(".content").attr("class").includes("is-subscriber")) {
-    //     console.log("subscriber")
-    //     $(".content").css("padding-top","0")
-    // }
-    if($(".content").attr("class").includes("is-subscriber")) {
-        console.log('logged in')
-    }else {
-        $(".content").css("margin-top",$(".header").height())
-    }
-    $(window).trigger('resize');
+  // if ($(".content").attr("class").includes("is-subscriber")) {
+  //     console.log("subscriber")
+  //     $(".content").css("padding-top","0")
+  // }
+  if ($(".content").attr("class").includes("is-subscriber")) {
+    console.log('logged in')
+  } else {
+    $(".content").css("margin-top", $(".header").height())
+  }
+  $(window).trigger('resize');
 
-    AOS.init({
-        easing: "ease",
-        once: "true",
-        duration: 2000,
-        offset: -50,
-        anchor: "center-bottom",
-        disable: 'mobile'
+  AOS.init({
+    easing: "ease",
+    once: "true",
+    duration: 2000,
+    offset: -50,
+    anchor: "center-bottom",
+    disable: 'mobile'
+  });
+
+
+  var winScrollTop=0;
+
+  $.fn.is_on_screen = function(){
+      var win = $(window);
+      var viewport = {
+          top : win.scrollTop(),
+          left : win.scrollLeft()
+      };
+      //viewport.right = viewport.left + win.width();
+      viewport.bottom = viewport.top + win.height();
+
+      var bounds = this.offset();
+      //bounds.right = bounds.left + this.outerWidth();
+      bounds.bottom = bounds.top + this.outerHeight();
+
+      return (!(viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+  };
+
+  function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.illustration-wrapper').each(function(){
+
+    	 if ($(this).is_on_screen()) {
+    	 			var firstTop = $(this).offset().top;
+            var $span = $(this);
+            var moveTop = (firstTop-winScrollTop)*0.2 //speed;
+            // $span.css("transform","translateY("+-moveTop+"px)");
+            $span.css("top", (moveTop));
+       }
+
     });
+  }
 
-    $(window).bind('scroll', function(e) {
-        parallaxScroll();
-    });
+  $(window).scroll(function(e){
+    winScrollTop = $(this).scrollTop();
+    parallax();
+  });
 
-    function parallaxScroll() {
-        var scrolled = $(window).scrollTop();
-        $('.illustration-wrapper').css('top', (0 - (scrolled * .1)) + 'px');
-    }
+
+
 });
 
 $(window).resize(function() {
-    if ($(window).width() <= 520) {
-        $(".heroNav").hover(
-            function() {
-                $(".parts").css("display", "block");
-                setTimeout(function() {
-                    $(".parts").addClass("dropDown");
-                }, 100);
-            },
-            function() {
-                $(".parts").removeClass("dropDown");
-                setTimeout(function() {
-                    $(".parts").css("display", "");
-                }, 400);
-            }
-        );
-    }
+  if ($(window).width() <= 520) {
+    $(".heroNav").hover(
+      function() {
+        $(".parts").css("display", "block");
+        setTimeout(function() {
+          $(".parts").addClass("dropDown");
+        }, 100);
+      },
+      function() {
+        $(".parts").removeClass("dropDown");
+        setTimeout(function() {
+          $(".parts").css("display", "");
+        }, 400);
+      }
+    );
+  }
 })
 
 
@@ -56,50 +89,50 @@ $(window).scroll(_.throttle(slowscroll, 500));
 var lastScrollTop = 0;
 
 function slowscroll() {
-    var st = $(this).scrollTop();
-    if ($(window).scrollTop() > $(window).height()) {
-        if (st > lastScrollTop) {
-            if ($(".header").attr("class").includes("scrollIn")) {
-                $(".header").addClass('scrollOut');
-                setTimeout(function() {
-                    $(".header").removeClass("scrollIn").removeClass('scrollOut');
-                }, 500);
-            }
-        } else {
-            $(".header").addClass("scrollIn")
-        }
-    }
-    lastScrollTop = st;
-
-
-    // console.log($(".footerNav").offset().top - $(window).height()*1.5, $(window).scrollTop())
-    if ($(".byline-section").offset().top + $(window).height() / 2 <= $(window).scrollTop() && $(".footerNav").offset().top - $(window).height() * 1.5 > $(window).scrollTop()) {
-
-        if ($(".stickynav").length < 1) {
-            $("body").append('<div class="stickynav"><div class="stickyheader">Undercover<br>Gangster</div><div class="menu"><div class="part"><div class="name">In Their Fathers’ Footsteps</div><div class="pubdate">Nov. 13</div></div><div class="part notready"><div class="name">Portrait of a Suspect</div><div class="pubdate">Nov. 14</div></div><div class="part notready"><div class="name">Another Twist, Another Crime</div><div class="pubdate">Nov. 15</div></div><div class="part notready"><div class="name">Interrogation</div><div class="pubdate">Nov. 16</div></div><div class="part notready"><div class="name">The Reckoning</div><div class="pubdate">Nov. 17</div></div></div></div>')
-        }
-        $(".stickynav").hover(
-            function() {
-                $(this).find(".menu").addClass("stickyShow");
-                setTimeout(function() {
-                    $(".stickyShow").css("opacity", "1");
-                }, 100);
-
-            },
-            function() {
-                setTimeout(function() {
-                    $(".stickyShow").removeClass("stickyShow");
-                }, 100);
-                $(".stickyShow").css("opacity", "");
-
-            }
-        );
-    } else {
-
+  var st = $(this).scrollTop();
+  if ($(window).scrollTop() > $(window).height()) {
+    if (st > lastScrollTop) {
+      if ($(".header").attr("class").includes("scrollIn")) {
+        $(".header").addClass('scrollOut');
         setTimeout(function() {
-                $(".stickynav").remove();
-        }, 1000);
-        $(".stickyheader").addClass("stickyOut");
-
+          $(".header").removeClass("scrollIn").removeClass('scrollOut');
+        }, 500);
+      }
+    } else {
+      $(".header").addClass("scrollIn")
     }
+  }
+  lastScrollTop = st;
+
+
+  // console.log($(".footerNav").offset().top - $(window).height()*1.5, $(window).scrollTop())
+  if ($(".byline-section").offset().top + $(window).height() / 2 <= $(window).scrollTop() && $(".footerNav").offset().top - $(window).height() * 1.5 > $(window).scrollTop()) {
+
+    if ($(".stickynav").length < 1) {
+      $("body").append('<div class="stickynav"><div class="stickyheader">Undercover<br>Gangster</div><div class="menu"><div class="part"><div class="name">In Their Fathers’ Footsteps</div><div class="pubdate">Nov. 13</div></div><div class="part notready"><div class="name">Portrait of a Suspect</div><div class="pubdate">Nov. 14</div></div><div class="part notready"><div class="name">Another Twist, Another Crime</div><div class="pubdate">Nov. 15</div></div><div class="part notready"><div class="name">Interrogation</div><div class="pubdate">Nov. 16</div></div><div class="part notready"><div class="name">The Reckoning</div><div class="pubdate">Nov. 17</div></div></div></div>')
+    }
+    $(".stickynav").hover(
+      function() {
+        $(this).find(".menu").addClass("stickyShow");
+        setTimeout(function() {
+          $(".stickyShow").css("opacity", "1");
+        }, 100);
+
+      },
+      function() {
+        setTimeout(function() {
+          $(".stickyShow").removeClass("stickyShow");
+        }, 100);
+        $(".stickyShow").css("opacity", "");
+
+      }
+    );
+  } else {
+
+    setTimeout(function() {
+      $(".stickynav").remove();
+    }, 1000);
+    $(".stickyheader").addClass("stickyOut");
+
+  }
 }
