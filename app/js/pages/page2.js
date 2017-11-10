bfd3ac992bbd4be57b0189651e2e183b65634429
Garland <garlandpotts1@gmail.com>
2017-11-10 17:18:03 -0500

    Updated part 2

--- app/js/pages/page2.js
+++ app/js/pages/page2.js
@@ -1,20 +1,21 @@
 console.log('page 2')
 
+$(document).ready(function() {
+    // $(window).resize(function() {
+    if ($(window).width() > 1000) {
+        var distance = $('#illustration_3-trigger').offset().top,
+            $window = $(window),
+            release3 = $("#illustration_3-trigger-release").offset().top;
+        $window.scroll(function() {
 
-// $(window).resize(function() {
-  if ($(window).width() > 1000) {
-    var distance = $('#illustration_3-trigger').offset().top,
-      $window = $(window),
-      release3 = $("#illustration_3-trigger-release").offset().top;
-    $window.scroll(function() {
-
-      console.log(distance, $window.scrollTop(), release3);
-      if ($window.scrollTop() >= distance && $window.scrollTop() <= release3) {
-        console.log("fire");
-        $('.sticky').fadeIn("slow");
-      } else {
-        $('.sticky').fadeOut("slow");
-      }
-    });
-  }
+            console.log(distance, $window.scrollTop(), release3);
+            if ($window.scrollTop() >= distance && $window.scrollTop() <= release3) {
+                console.log("fire");
+                $('.sticky').fadeIn("slow");
+            } else {
+                $('.sticky').fadeOut("slow");
+            }
+        });
+    }
+});
 // });
