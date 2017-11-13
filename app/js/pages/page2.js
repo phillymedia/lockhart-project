8a0a519a6eca374d45fa90e86ebb0c6e00b34990
jared-whalen <jwhalen@philly.com>
2017-11-13 09:37:25 -0500

    Changes

--- app/js/pages/page2.js
+++ app/js/pages/page2.js
@@ -1,10 +1,14 @@
 console.log('page 2')
 
 // $(window).resize(function() {
-  if ($(window).width() > 1000) {
+$(document).ready(function() {
+
+console.log("test " + $('#illustration_3-trigger').offset().top);
+
     var distance = $('#illustration_3-trigger').offset().top,
       $window = $(window),
       release3 = $("#illustration_3-trigger-release").offset().top;
+  if ($(window).width() > 1000) {
     $window.scroll(function() {
       console.log(distance, $window.scrollTop(), release3);
       if ($window.scrollTop() >= distance && $window.scrollTop() <= release3) {
@@ -15,4 +19,8 @@ console.log('page 2')
       }
     });
   }
+});
+
+
+
 // });
